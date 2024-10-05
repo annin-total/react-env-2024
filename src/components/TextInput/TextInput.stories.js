f43602eef367d05e11951a9fd777163db9e16b0c
yuki-2337 <130845842+yuki-2337@users.noreply.github.com>
2024-10-05 18:28:12 +0900

    Update サイト公開

---
+++ src/components/TextInput/TextInput.stories.js
@@ -0,0 +1,10 @@
+import TextInput from "./TextInput";
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: "TextInput",
+    component: TextInput,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ["autodocs"],
+};
+export default meta;
+export const Default = {};
